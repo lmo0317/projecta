f1861894f4107284d8d410761f7f9b5c6f100e34
lmo0317 <lmo0317@naver.com>
2022-07-04 23:43:01 +0900

    EventManager 추가중

---
+++ Assets/MyProject/Scripts/Event/EventManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class EventManager : MonoBehaviour
+{
+    public static UniRx.Subject<int> animationEvent = new ();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/MyProject/Scripts/UI/DebugUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI() // deprecated, use ordinary .UI now available in Unity
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 100), "Debug!"))
+        {
+            EventManager.animationEvent.OnNext(10);
+        }
+    }
+}
